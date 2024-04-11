da65d9dd95845c126d9eeb66b9e3f0524cf667d7
BDCasper <d.belkairov@gmail.com>
2024-04-11 13:53:57 +0500

    pochti gotov

--- src/components/Podpiska/podpiska.tsx
+++ src/components/Podpiska/podpiska.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./podpiska.css"
 
 interface Props {
@@ -7,41 +7,89 @@ interface Props {
 
 export default function Podpiska({setPopOpen}:Props) {
 
+    const [checkPath, setCheckPath] = useState<boolean>(false);
+
+    useEffect(() => {
+        (
+            async() => {
+                if(window.location.pathname === '/subscription')
+                    setCheckPath(true);
+                else setCheckPath(false);
+            }
+        )()
+    },[window.location.pathname])
+
 
     return(
-        <div className="sub">
-            <div className="sub-block-close" onClick={() => setPopOpen(false)}></div>
-            <div className="sub-block">
-            <div className="sub-ramka">
-                <div className="sub-upper-text">
-                    <div className="sub-title">ПОДПИСКА</div>
-                    <div className="sub-status">Неактивировано</div>
-                    <div className="sub-descr">Безлимитный доступ ко всем урокам</div>
-                </div>
-                <div className="sub-lenta">
-                    <div className="sub-lenta-center"><span className="sub-lenta-center-text">33$ в месяц</span></div>
-                    <div className="sub-lenta-sides">
-                        <div className="sub-lenta-left"></div>
-                        <div className="sub-lenta-right"></div>
+        <>
+        {checkPath === true ? 
+            <div className="sub-main">
+                    <div className="sub-ramka">
+                        <div className="sub-upper-text">
+                            <div className="sub-title">ПОДПИСКА</div>
+                            <div className="sub-status">Неактивировано</div>
+                            <div className="sub-descr">Безлимитный доступ ко всем урокам</div>
+                        </div>
+                        <div className="sub-lenta">
+                            <div className="sub-lenta-center"><span className="sub-lenta-center-text">33$ в месяц</span></div>
+                            <div className="sub-lenta-sides">
+                                <div className="sub-lenta-left"></div>
+                                <div className="sub-lenta-right"></div>
+                            </div>
+                        </div>
+                        <div className="sub-numbers">
+                                <ul>
+                                    <li>22 курса</li>
+                                    <li>200 уроков</li>
+                                    <li>4000 задач</li>
+                                </ul>
+                        </div>
+                        <div className="sub-lower-text">
+                            <ul>
+                                <li>Для взрослых</li>
+                                <li>Для детей</li>
+                                <li>Для продолжающих</li>
+                            </ul>
+                        </div>
+                        <button className="sub-button">Оформить подписку</button>
                     </div>
-                </div>
-                <div className="sub-numbers">
+            </div>
+        :
+            <div className="sub">
+                <div className="sub-block-close" onClick={() => setPopOpen(false)}></div>
+                <div className="sub-block">
+                <div className="sub-ramka">
+                    <div className="sub-upper-text">
+                        <div className="sub-title">ПОДПИСКА</div>
+                        <div className="sub-status">Неактивировано</div>
+                        <div className="sub-descr">Безлимитный доступ ко всем урокам</div>
+                    </div>
+                    <div className="sub-lenta">
+                        <div className="sub-lenta-center"><span className="sub-lenta-center-text">33$ в месяц</span></div>
+                        <div className="sub-lenta-sides">
+                            <div className="sub-lenta-left"></div>
+                            <div className="sub-lenta-right"></div>
+                        </div>
+                    </div>
+                    <div className="sub-numbers">
+                            <ul>
+                                <li>22 курса</li>
+                                <li>200 уроков</li>
+                                <li>4000 задач</li>
+                            </ul>
+                    </div>
+                    <div className="sub-lower-text">
                         <ul>
-                            <li>22 курса</li>
-                            <li>200 уроков</li>
-                            <li>4000 задач</li>
+                            <li>Для взрослых</li>
+                            <li>Для детей</li>
+                            <li>Для продолжающих</li>
                         </ul>
+                    </div>
+                    <button className="sub-button">Оформить подписку</button>
                 </div>
-                <div className="sub-lower-text">
-                    <ul>
-                        <li>Для взрослых</li>
-                        <li>Для детей</li>
-                        <li>Для продолжающих</li>
-                    </ul>
                 </div>
-                <button className="sub-button">Оформить подписку</button>
             </div>
-            </div>
-        </div>
+        }
+        </>
     );
 }
