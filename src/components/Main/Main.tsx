ff133a61a8d241c3125975e49080a5519e366a23
BDCasper <d.belkairov@gmail.com>
2024-08-08 20:02:32 +0500

    logic structure rebuild

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -75,7 +75,7 @@ export default function Main({inp, user, arrayOfSolved}:MainProps) {
       useEffect(() => {
         (
           async() => {
-            if(solvedMap.size > 0 && firstReload === false) 
+            if(localStorage.getItem('user_id') !== null && solvedMap.size > 0 && firstReload === false) 
             window.location.reload();
             setFirstReload(true);
           }
